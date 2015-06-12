e0914d9d3370d696e316afb50beff028bc837d22
Padmakar Ojha <padmakar.ojha@makemytrip.com>
2015-06-12 18:07:28 +0530

    code updated

--- csv_encrypt.py
+++ csv_encrypt.py
@@ -9,28 +9,41 @@ def list_files(path):
             files.append(name)
     return files
 
-csv_dir = os.getcwd()
-csv_files = list_files(os.path.join(csv_dir, "csv_sample"))
-columns_to_encrypt = ['Time','User ID']
-cypher_obj = AESCipher("2805")
+# Configuration
+csv_dir = "C:\Users\mmt5267\Desktop\BLR_Data\Hotel_Data\split"
+csv_files = list_files(os.path.join(csv_dir))
+cypher_obj = AESCipher('2805')
+header_row = ['col1', 'col2', 'col3', 'col4', 'col5', 'col6', 'col7',
+              'col8', 'col9', 'col10', 'col11', 'col12', 'col13', 'col14',
+              'col15']
+columns_to_encrypt = ['col6', 'col7']
 
 encrpyt_mapping = []
 for col in columns_to_encrypt:
     encrpyt_mapping.append(col)
-    encrpyt_mapping.append(col + " encrpted_value")
+    encrpyt_mapping.append(str(col) + " encrypted_value")
 
 for csv_file in csv_files:
-    mapping_df = DataFrame(columns=(encrpyt_mapping))
-    data =  read_csv(os.path.join(csv_dir, "csv_sample",csv_file))
+    print "Parsing csv file  %s" % csv_file
+    mapping_df = DataFrame(columns=encrpyt_mapping)
+    data = read_csv(os.path.join(csv_dir, csv_file), error_bad_lines=False, names=header_row, dtype='unicode')
+    print "Encrypting of data in csv file %s is starting" % csv_file
     for index, row in data.iterrows():
-        i=0
+        # i = 0
+        # print(index)
         for col in columns_to_encrypt:
-            encoded = cypher_obj.encrypt(row[col])
-            data.loc[index,col] = encoded
-            mapping_df.loc[index,encrpyt_mapping[i]] = row[col]
-            mapping_df.loc[index,encrpyt_mapping[i+1]] = encoded
-            i += 2
-            #decrypt_user = cypher_obj.decrypt(encoded)
-            #print("decrypted : %s" % decrypt_user)
-    data.to_csv(os.path.join(csv_dir, "csv_sample","enc_" + csv_file), encoding='utf-8')
-    mapping_df.to_csv(os.path.join(csv_dir, "csv_sample","mapping_" + csv_file), encoding='utf-8')
+            if row[col] == 'NaN':
+                data.loc[index, col] = row[col]
+            else:
+                if isinstance(row[col], float):
+                    row[col] = int(row[col])
+                encoded = cypher_obj.encrypt(row[col])
+                data.loc[index, col] = encoded
+            # print(encoded + " " + cypher_obj.decrypt(encoded))
+            # mapping_df.loc[index, encrpyt_mapping[i]] = row[col]
+            # mapping_df.loc[index, encrpyt_mapping[i+1]] = encoded
+            # i += 2
+    print "Encrypting of data in csv file %s is completed" % csv_file
+    print "Writing CSV file for encrypted data"
+    data.to_csv(os.path.join(csv_dir, "enc_" + csv_file), encoding='utf-8')
+    # mapping_df.to_csv(os.path.join(csv_dir, "mapping_" + csv_file), encoding='utf-8')
